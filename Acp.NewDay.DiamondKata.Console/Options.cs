8cd15ec55b713e6a8a355f87d7c43c8340ba924d
Andrei Ciprian Popescu <acpopescu@outlook.com>
2024-02-19 17:33:28 +0200

    add console project

---
+++ Acp.NewDay.DiamondKata.Console/Options.cs
@@ -0,0 +1,13 @@
+﻿
+// using Console = global::System.Console;
+
+namespace Acp.NewDay.DiamondKata.CLI;
+
+internal class Options
+{
+    [Value(0, Required = true, HelpText = "Diamond character to iterate to")]
+    public char Letter { get; set; }
+
+    [Option(longName: "whitespace", shortName: 'w', Default = '_', Required = false)]
+    public char WhiteSpaceChar { get; set; }
+}
---
+++ Acp.NewDay.DiamondKata.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// using Console = global::System.Console;
+
+namespace Acp.NewDay.DiamondKata.CLI;
+internal static class Program
+{
+    private static void Main(string[] args)
+    {
+        Parser.Default.ParseArguments<Options>(args)
+            .WithParsed(options =>
+            {
+                var printer = new DiamondKata(new LineBuilder());
+                var diamond = printer.Print(options.Letter);
+                Console.WriteLine(diamond);
+            })
+            .WithNotParsed(errors =>
+            {
+                foreach (var error in errors)
+                {
+                    Console.WriteLine(error);
+                }
+            });
+    }
+}
